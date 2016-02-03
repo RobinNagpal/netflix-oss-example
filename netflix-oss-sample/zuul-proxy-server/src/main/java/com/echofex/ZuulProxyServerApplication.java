8cc622004b52cc78b805415e96984ee89bee64a9
Robin Nagpal <robin.nagpal@metlife.com>
2016-02-03 12:29:36 -0500

    create seperate project for zuul proxy server

---
+++ netflix-oss-sample/zuul-proxy-server/src/main/java/com/echofex/ZuulProxyServerApplication.java
@@ -0,0 +1,17 @@
+package com.echofex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EntityScan("com.echofex")
+@EnableZuulProxy
+public class ZuulProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServerApplication.class, args);
+	}
+}
